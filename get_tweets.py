cd44ab5428b77a3adc36e0bad6f6ad067bcc3d40
Ihjas <ihjasdev@gmail.com>
2022-12-31 10:18:17 +0530

    first commit

---
+++ get_tweets.py
@@ -0,0 +1,23 @@
+import snscrape.modules.twitter as sntwitter
+import pandas as pd
+
+
+query ="(from:justinbieber) until:2022-12-31 since:2021-01-01"
+tweets=[]
+limit =100
+
+for tweet in sntwitter.TwitterSearchScraper(query).get_items():
+
+    if len(tweets)== limit:
+     break
+    else:
+        tweets.append([tweet.date,tweet.user.username,tweet.content])
+
+#Put Column Name
+df=pd.DataFrame(tweets, columns=['Tweet Date','User','Tweet'])
+
+print(df)
+print("-------------Converting into csv file----------")
+
+#export the dataframe to csv file
+df.to_csv(r'justin_tweets.csv', index=False)
