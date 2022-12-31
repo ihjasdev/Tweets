c9e2f790410167a1e4cc775f1b3b061da24d4d7f
Ihjas <ihjasdev@gmail.com>
2022-12-31 10:42:17 +0530

    user3

--- get_tweets.py
+++ get_tweets.py
@@ -2,7 +2,7 @@ import snscrape.modules.twitter as sntwitter
 import pandas as pd
 
 
-query ="(from:Cristiano) until:2022-12-31 since:2021-01-01"
+query ="(from:UMadhavii) until:2022-12-31 since:2020-01-01"
 tweets=[]
 limit =100
 
@@ -20,4 +20,4 @@ print(df)
 print("-------------Converting into csv file----------")
 
 #export the dataframe to csv file
-df.to_csv(r'cristiano_tweets.csv', index=False)
+df.to_csv(r'Madhavi_tweets.csv', index=False)
