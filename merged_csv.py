76f56ef42a50a39680666828400d3fc29fcdae59
Ihjas <ihjasdev@gmail.com>
2023-01-01 22:49:59 +0530

    merged all datafiles into one

--- merged_csv.py
+++ merged_csv.py
@@ -1,10 +1,10 @@
 import pandas as pd
 from glob import glob
 
-data_files=sorted(glob('data*.csv'))
+data_files=sorted(glob('Tweets\data*.csv'))
 data_files
 
 mergeddata = pd.concat(pd.read_csv(datafile).assign(sourcefilename= datafile)
                 for datafile in data_files)
 
-mergeddata.to_csv('merged.csv')
+mergeddata.to_csv('Tweets\merged.csv')
