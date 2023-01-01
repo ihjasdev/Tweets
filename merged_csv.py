d12b95d770701eb92a4a136c5a55f5f5bd3a2ce6
Ihjas <ihjasdev@gmail.com>
2023-01-01 22:57:03 +0530

    merged csv

--- merged_csv.py
+++ merged_csv.py
@@ -6,5 +6,5 @@ data_files
 
 mergeddata = pd.concat(pd.read_csv(datafile).assign(sourcefilename= datafile)
                 for datafile in data_files)
-
+#after the merge all data and save it a newfile as merged.csv
 mergeddata.to_csv('Tweets\merged.csv')
