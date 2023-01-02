adcd966a5bdb6658fc4511c749f59f04c0c863f4
Ihjas <ihjasdev@gmail.com>
2023-01-02 23:06:09 +0530

    remove special char

--- del_character.py
+++ del_character.py
@@ -4,11 +4,13 @@ import re
 # Read in the CSV file
 df = pd.read_csv("Tweets/new_file.csv")
 
-# Remove special characters
-df = df.applymap(lambda x: re.sub(r'[^\w\s]', '', x))
-
 # Remove URLs
 df = df.applymap(lambda x: re.sub(r'https?:\/\/.*[\r\n]*', '', x))
 
+# Remove special characters
+df = df.applymap(lambda x: re.sub(r'[^\w\s]', '', x))
+
+
+
 # Save the resulting dataframe to a new CSV file
-df.to_csv("Tweets/del_char_file.csv", index=False)
+df.to_csv("Tweets/remove_char_file.csv", index=False)
